dc4c811a4e1b90ad6e137bbda52946ef3a83b78f
Daria Buravleva <luna_ortus@mail.ru>
2024-03-14 22:29:35 +0300

    Расчеты

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
+
 public class Main {
+    //Часть: 2. Практическое задачние 8.
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма чисел="+sum);
+
+        int raznost = firstNumber - secondNumber;
+        System.out.println("Разность чисел="+raznost);
+
+        int proizvedenie = firstNumber * secondNumber;
+        System.out.println("Произведение чисел="+proizvedenie);
+
+        int chastnoe = firstNumber / secondNumber;
+        System.out.println("Частное чисел="+chastnoe);
     }
 }
